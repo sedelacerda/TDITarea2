8ce7a406ea0d8e998162271b12c84b1cbd4d6148
SEBASTIAN DE LA CERDA <sedelacerda@uc.cl>
2017-03-27 19:31:56 -0300

    Inicializacion

---
+++ app/controllers/usuario_controller.rb
@@ -0,0 +1,16 @@
+class UsuarioController < ApplicationController
+  def new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def index
+  end
+
+  def destroy
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  get 'usuario/new'
+
+  get 'usuario/show'
+
+  get 'usuario/edit'
+
+  get 'usuario/index'
+
+  get 'usuario/destroy'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
